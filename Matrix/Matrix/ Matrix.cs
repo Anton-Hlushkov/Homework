040799e3f7ecec0ef72bf676ccb0ecc767f064e1
Anton Hlushkov <anton.hlushkov@gmail.com>
2019-11-14 15:00:33 +0300

    matrix commit

---
+++ Matrix/Matrix/ Matrix.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Matrix
+{
+    public class Matrix <T>
+    {
+		int size;
+		T *[]row = new T [size];
+		T []columns = new T [size];
+		public Matrix(int a, T*[]b)
+		{
+			size = a;
+			columns = b;
+
+		}
+    }
+}
