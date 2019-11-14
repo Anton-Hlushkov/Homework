dee8209a0a0ccf4f5f5015d851b51d203856f58d
Anton Hlushkov <anton.hlushkov@gmail.com>
2019-11-14 15:05:36 +0300

    trangles commit

---
+++ TranglesEqual/TranglesEqual/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace TranglesEqual
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+        public Point(int x1, int y1) //constuctor
+        {
+            x = x1;
+            y = y1;
+        }
+        public override string ToString()
+        {
+            return "x= " + x + "y= " + y;
+        }
+
+    }
+}
---
+++ TranglesEqual/TranglesEqual/Triangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace TranglesEqual
+{
+    public class Triangle
+    {
+        Point a;
+        Point b;
+        Point c;
+        double side1;
+        double side2;
+        double side3;
+        public Triangle(Point a1, Point b1,Point c1)
+        {
+            a = a1;
+            b = b1;
+            c = c1;
+            double side1 = Math.Sqrt((a1.x - b1.x)*(a1.x - b1.x)+ (a1.y - b1.y)*(a1.y - b1.y));
+            double side2 = Math.Sqrt((b1.x - c1.x) * (b1.x - c1.x) + (b1.y - c1.y) * (b1.y - c1.y));
+            double side3 = Math.Sqrt((a1.x - c1.x) * (a1.x - c1.x) + (a1.y - c1.y) * (a1.y - c1.y));
+        }
+        
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            Triangle m = obj as Triangle; // return null if object isnt a triangle
+            if (m as Triangle == null)
+                return false;
+            
+
+
+
+            return (m.side1 == this.side1 && m.side2 == this.side2 && m.side3 == this.side3)||
+                (m.side1 == this.side1 && m.side2 == this.side3 && m.side3 == this.side2)||
+                (m.side1 == this.side2 && m.side2 == this.side1 && m.side3 == this.side3)||
+                (m.side1 == this.side2 && m.side2 == this.side3 && m.side3 == this.side1)||
+                (m.side1 == this.side3 && m.side2 == this.side2 && m.side3 == this.side1)||
+                (m.side1 == this.side3 && m.side2 == this.side1 && m.side3 == this.side2);
+        }
+        
+    }
+}
