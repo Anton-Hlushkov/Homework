4c8da1b0c6b8a68713390de08b66bb386ff65df5
Anton Hlushkov <anton.hlushkov@gmail.com>
2019-11-18 17:56:18 +0300

    stringcommit

--- String/StringTest/StringMetodTest.cs
+++ String/StringTest/StringMetodTest.cs
@@ -14,8 +14,39 @@ namespace StringTest
             int actual = TestString.Program.MaxAmountOfNumbers(testString);
             Assert.AreEqual(expected, actual);
 
+            testString = "b";//case of string with one not numeric symbol 
+            actual = TestString.Program.MaxAmountOfNumbers(testString);
+            Assert.AreEqual(expected, actual);
 
+            testString = " ";
+            actual = TestString.Program.MaxAmountOfNumbers(testString);
+            Assert.AreEqual(expected, actual);
+
+            expected = 1;
+
+            testString = "1";
+            actual = TestString.Program.MaxAmountOfNumbers(testString);
+            Assert.AreEqual(expected, actual);
+
+            testString = "21";
+            actual = TestString.Program.MaxAmountOfNumbers(testString);
+            Assert.AreEqual(expected, actual);
+
+            testString = "ad12d";
+            actual = TestString.Program.MaxAmountOfNumbers(testString);
+            Assert.AreEqual(expected, actual);
+
+            expected = 2;
+
+            testString = "ad22d";
+            actual = TestString.Program.MaxAmountOfNumbers(testString);
+            Assert.AreEqual(expected, actual);
+
+            testString = "99";
+            actual = TestString.Program.MaxAmountOfNumbers(testString);
+            Assert.AreEqual(expected, actual);
 
         }
+       
     }
 }
